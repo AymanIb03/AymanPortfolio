678f4fe31469d88b96e464223e3848f4d0000feb
Ayman Ibnouennadre <Ayman.Ibnouennadre@gmail.com>
2024-11-15 12:10:37 +0100

    title update again

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Certificate from "./components/Certificate";
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Ayman Ibnouennadre</title>
+      </Helmet>
       <Navbar />
       <Home />
       <About />
